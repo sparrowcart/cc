f04a0b873d72dc56161ecff269f98c3155f0f6a5
sparrowcart <82755207+sparrowcart@users.noreply.github.com>
2023-05-07 22:01:48 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,99 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>ccc</title>
+
+<!-- CSS FILES -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700;900&display=swap"
+    rel="stylesheet">
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-icons.css" rel="stylesheet">
+<link rel="stylesheet" href="css/aos.css">
+
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css"
+    integrity="sha512-h5/L9lGjLnaoHUZcOtxf0XX4GKjwN/CFSt8PfR/2Q6/+ev9M9WqDbTnIdyJ1lHsgxU+NNrQo8rBcZz+aFmX9oA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+    integrity="sha512-BTyK2jikJir3qA3lxZLM/u+0iBUsyHjE7OktgmeR0wLTFouevBJ97xk5S5N21ZiEGvgb1jKX9Q4Q4iVTEDdnTw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.min.js"
+    integrity="sha512-ysHv6Q2vm6fX9/r8qTcT0v6wJ1rlFzAAr8rLcO+UmmCzhODhMlu9DT8DdqwYjGVwCY1JChzZ/qCfZQjnbZ+JlQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<link href="https://drive.google.com/uc?id=1lBZELNfutSJb5t8m7-cGWtRXpPI8jque" rel="stylesheet">
+
+
+<link rel="stylesheet" href="memo.css">
+
+<link rel="stylesheet" href="newContent.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link href="css/templatemo-nomad-force.css" rel="stylesheet">
+<link rel="icon" href="" type="image/x-icon">
+
+
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12">
+                <h5 class="text-white">
+                    <i class="bi-geo-alt-fill me-2"></i>
+                    State of Rio de Janeiro, Brazil
+                </h5>
+
+                <a href="mailto:info@company.com" class="custom-link mt-3 mb-5">
+                    info@company.com
+                </a>
+            </div>
+
+            <div class="col-6">
+                < class="copyright-text mb-0">Copyright © Nomad Force 2021
+
+                    <br><br>Design: <a href="https://templatemo.com/page/1" target="_parent">TemplateMo</a><br><br>
+
+                    Distributed By: <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+                    </p>
+
+            </div>
+
+            <div class="col-lg-3 col-5 ms-auto">
+                <ul class="social-icon">
+                    <li><a href="#" class="social-icon-link bi-facebook"></a></li>
+
+                    <li><a href="#" class="social-icon-link bi-twitter"></a></li>
+
+                    <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
+
+                    <li><a href="#" class="social-icon-link bi-instagram"></a></li>
+
+                    <li><a href="#" class="social-icon-link bi-youtube"></a></li>
+                </ul>
+            </div>
+
+        </div>
+        </section>
+</footer>
+
+<!-- JAVASCRIPT FILES -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/magnific-popup-options.js"></script>
+<script src="js/scrollspy.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/custom.js"></script>
+
+<script src="newContent.js"></script>
---
+++ includes/header.php
@@ -0,0 +1,127 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ccc</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700;900&display=swap"
+        rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css"
+        integrity="sha512-h5/L9lGjLnaoHUZcOtxf0XX4GKjwN/CFSt8PfR/2Q6/+ev9M9WqDbTnIdyJ1lHsgxU+NNrQo8rBcZz+aFmX9oA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-BTyK2jikJir3qA3lxZLM/u+0iBUsyHjE7OktgmeR0wLTFouevBJ97xk5S5N21ZiEGvgb1jKX9Q4Q4iVTEDdnTw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.min.js"
+        integrity="sha512-ysHv6Q2vm6fX9/r8qTcT0v6wJ1rlFzAAr8rLcO+UmmCzhODhMlu9DT8DdqwYjGVwCY1JChzZ/qCfZQjnbZ+JlQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <link href="https://drive.google.com/uc?id=1lBZELNfutSJb5t8m7-cGWtRXpPI8jque" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="memo.css">
+
+    <link rel="stylesheet" href="newContent.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link href="css/templatemo-nomad-force.css" rel="stylesheet">
+    <link rel="icon" href="" type="image/x-icon">
+
+
+
+    <section class="hero" id="hero">
+        <div class="heroText">
+
+            <p class="top-pra">Let us take over your</p>
+
+
+            <h2 id="changingword" class="change-text mt-5 mb-lg-4"></h2>
+
+
+            <!--                 <p class="top-p"> Keeping your business running so you can focus on what you do best
+                </p> -->
+        </div>
+
+
+
+        <div class="videoWrapper">
+            <video autoplay="" loop="" muted="" class="custom-video"
+                poster="videos/792bd98f3e617786c850493560e11c45.jpg">
+                <source src="videos/814dc43e870597176cad645798825c03.mp4" type="video/mp4">
+
+                Your browser does not support the video tag.
+            </video>
+        </div>
+
+        <div class="overlay"></div>
+    </section>
+
+    <nav class="navbar navbar-expand-lg bg-light shadow-lg">
+        <div class="container">
+            <a class="navbar-brand" href="http://creativecodecorner.com/">
+                <!-- <strong>Creative Code Corner</strong> -->
+                <img src="logo.png" class='logo-1' alt="" srcset="">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon burge"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#hero">Home</a>
+                    </li>
+
+
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Portfolio</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#vision">Services</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#price">Pricing</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+    <script src="js/scrollspy.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/custom.js"></script>
+
+    <script src="newContent.js"></script>
+
+
+</head>
